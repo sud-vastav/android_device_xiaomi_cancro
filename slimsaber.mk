38c3b37e359bf090bb63226146113ae6cce6253e
shahan-mik3 <shahan.mike13@gmail.com>
2015-07-08 16:03:16 +0530

    slim-ify
    slim-ify

--- slimsaber.mk
+++ slimsaber.mk
@@ -23,12 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slimsaber/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slimsaber/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_cancro
+PRODUCT_NAME := slimsaber_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
